b72050db8deed73b61a1c79b6b9114ffb4b20870
songzhao <songzhao@shangyoo.com>
2023-06-20 20:07:29 +0800

    common-lang

---
+++ common-lang/src/main.rs
@@ -0,0 +1,28 @@
+fn main() {
+    // let mut s = String::from("value");
+    // s += "a"; 
+    // println!("{s}");
+    // let condition = true;
+    // let n = if condition {5} else {6};
+    // println!("{n}");
+    // let condition = false;
+    // println!("{condition}");
+    // let mut n = 10;
+    // while n != 0 {
+    //     println!("{n}");
+    //     n -= 1;
+    // }
+    // let num = loop {
+    //     if n == 0 {
+    //         break n;
+    //     }
+    //     println!("{n}");
+    //     n -= 1;
+    // };
+    // println!("{num}");
+    
+    let arr = [10,1,2,3];
+    for item in arr {
+        println!("{item}");
+    }
+}
